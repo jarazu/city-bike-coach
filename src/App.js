8c1a153639c364f987702415c037fdbf64517b10
R041803052 <R041803052@DEVELOPER-24>
2021-10-04 10:59:46 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Services from './components/Services/Services';
-
+import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
       <Router>
         <Header/>
         <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
           <Route exact path="/home">
             <Home/>
           </Route>
           <Route exact path="/services">
             <Services/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route path="*">
+            <NotFound/>
           </Route>
         </Switch>
       </Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Alert, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from '../../logo.svg';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <Link to="/home">Home</Link> &nbsp;
-          <Link to="/friends">Friends</Link>  &nbsp;
+            <img src={logo} alt="logo" className="logo"/>
+          <Link to="/home">Home</Link> &nbsp;
+          <Link to="/services">Services</Link>  &nbsp;
           <Link to="/about">About</Link> &nbsp;
-          <Link to="/about/culture">Culture</Link> &nbsp;
+          <Link to="/testimonials">Testimonials</Link> &nbsp;
         </div>
     );
 };
